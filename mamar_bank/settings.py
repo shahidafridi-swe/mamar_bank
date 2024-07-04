3938893ed7c6bc7fbd6ff45690018a913e79b4c9
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-07-04 17:13:00 +0600

    allowed host all

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
